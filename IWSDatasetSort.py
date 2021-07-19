c559e3a90b56271bf44c66b1559c42f032a24d7a
NanXiJun1st <gd632631@163.com>
2021-07-19 17:31:43 +0800

    增加分类选择

--- IWSDatasetSort.py
+++ IWSDatasetSort.py
@@ -10,9 +10,10 @@ def ChooseFolder(*args):
     layout = [[sg.Text('IWSDataset')], 
               [sg.Text('根目录',size=(15,1),key='-Folder-'),sg.InputText(),sg.FolderBrowse()],
               [sg.Text('选择文件',size=(15,1),key='-file-'),sg.InputText(),sg.FilesBrowse()],
-              [sg.Combo(['脑膜瘤','胶质瘤'],size=(10,1))],
-            #   [sg.popup_scrolled('Log',size=(80,None),key='-log-')],
-              [sg.Submit(),sg.Cancel()]
+              [sg.Text('病种类型',size=(15,1)),sg.Combo(['脑膜瘤','胶质瘤'],default_value = '脑膜瘤', size=(10,1),key='-DiseazeName-')],
+              [sg.Text('输出信息',size=(15,1)),sg.Combo(['数量','所在行','文件夹名称'],default_value='数量',size=(10,1),key='-LogData-')],
+              [sg.Submit(),sg.Cancel()],
+              [sg.Text('Log')]
              ]
 
     window = sg.Window('IWS DATASET',layout,location=(0,0))
@@ -22,15 +23,23 @@ def ChooseFolder(*args):
         if event in (sg.WIN_CLOSED,'Cancel'):
             break
         # print(f'You choose filenames{values[0]} and {values[1]}')
-        return values[1]
+        ReadXlsxFile(values[1],values['-DiseazeName-'],values['-LogData-'])
     window.close()
 
-def ReadXlsxFile(*args):
+def ReadXlsxFile(path,diseazeName,logData):
     wb = Workbook()
-    wb = load_workbook("./数据收集对应关系总表_20210628.xlsx")
+    wb = load_workbook(path)
     sheet = wb.active
-    print(sheet["A1"].value)
-
+    diseazeNameList = sheet['G']
+    num = 0
+    for i in diseazeNameList:
+        if i.value == diseazeName:
+            num+=1
+            if logData == '数量':
+                sg.Print(num)
+            if logData =='所在行':
+                sg.Print(i.row)
+            if logData == '文件夹名称':
+                sg.Print(sheet['A'+str(i.row)].value)
 
 ChooseFolder()
-ReadXlsxFile()
